fc5fdc3c8a037d2621e2082f280dbc4cea6a7b8e
eui20n <eui20n@naver.com>
2022-09-07 23:47:40 +0900

    시간 복잡도

--- 1주차/시간복잡도.py
+++ 1주차/시간복잡도.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 
 def sum_n(n):
@@ -16,17 +17,50 @@ def n_by_n(n):
     print(f"{n} x {n} = {n ** 2}")
 
 
+def binary_search(n):
+    """ 이분 탐색으로 O(logn)"""
+    start = 1
+    end = n
+    while True:
+        if start >= end:
+            break
+
+        mid = (start + end)//2
+
+        if mid > start:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+
 def main():
     """ 시간 별로 출력해줄 함수 """
     num_n = [10, 100, 1000, 10000, 100000]
-    print("2번 과제")
+
+    print("logn의 복잡도")
+    for n in num_n:
+        start_time = time.time()
+        binary_search(n)
+        end_time = time.time()
+        print(f"{n} time : {round(end_time - start_time, 2)}")
+
+    print("n의 시간 복잡도")
     for n in num_n:
         start_time = time.time()
         sum_n(n)
         end_time = time.time()
         print(f"{n} time : {round(end_time - start_time, 2)}")
 
-    print("3번 과제")
+    print("nlogn의 복잡도")
+    for n in num_n:
+        arr = [x for x in range(n)]
+        random.shuffle(arr)
+        start_time = time.time()
+        arr.sort()
+        end_time = time.time()
+        print(f"{n} time : {round(end_time - start_time, 2)}")
+
+    print("n^2의 시간 복잡도")
     for n in num_n:
         start_time = time.time()
         n_by_n(n)
