eed994f25f2fdb2abda23349b684d4f8561a2db0
eui20n <eui20n@naver.com>
2022-09-16 11:14:42 +0900

    3주차

--- 3주차/게임 개발.py
+++ 3주차/게임 개발.py
@@ -1,54 +1,67 @@
+
+
+def visited_init():
+    """ 방문 처리 리스트를 만드는 함수 """
+    visited = [[False for _ in range(C)] for _ in range(R)]
+
+    for x in range(R):
+        for y in range(C):
+            if board[x][y]:
+                visited[x][y] = True
+
+    return visited
+
+
+
 def game():
     """ 게임 개발 함수 """
-    # 북 동 남 서
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    direction = [3, 0, 1, 2]
+    # 서 남 동 북 서 남 동 북
+    dx = [0, 1, 0, -1, 0, 1, 0, -1]
+    dy = [-1, 0, 1, 0, -1, 0, 1, 0]
 
     x, y, d = start_x, start_y, start_d
 
-    cnt = 1
-    end_con = 0
+    visited = visited_init()
+    visited[x][y] = True
 
-    visited = [[False for _ in range(C)] for _ in range(R)]
-    visited[start_x][start_y] = True
+    cnt = 1
 
     while True:
-        if end_con == 4:
+        go_back = True
+
+        for z in range(d, d + 4):
+            nx = x + dx[z]
+            ny = y + dy[z]
+            if 0 > nx or nx >= R:
+                continue
+            if 0 > ny or ny >= C:
+                continue
+            if visited[nx][ny]:
+                continue
+            if board[nx][ny] == 1:
+                continue
+
+            x = nx
+            y = ny
+            cnt += 1
+            d = z - 4 if z >= 4 else z
+            visited[nx][ny] = True
+            go_back = False
             break
 
-        d = 0 if d >= 4 else d + 1
+        if go_back:
+            nx = x - dx[d]
+            ny = y - dy[d]
 
-        nx = x + dx[direction[d]]
-        ny = y + dy[direction[d]]
+            if board[nx][ny] == 1:
+                return cnt
 
-        if 0 > nx or nx >= R:
-            end_con += 1
-            continue
-        if 0 > ny or ny >= C:
-            end_con += 1
-            continue
-        if board[nx][ny] == 1:
-            end_con += 1
-            continue
-
-        x = nx
-        y = ny
-        cnt += 1
-        end_con = 0
-
-    print(cnt)
+            x = nx
+            y = ny
 
 
 if __name__ == "__main__":
     R, C = map(int, input().split())
     start_x, start_y, start_d = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(R)]
-    game()
-
-
-# 0 : 북쪽
-# 1 : 동쪽
-# 2 : 남족
-# 3 : 서쪽
-# 왼쪽으로 돔
+    print(game())
