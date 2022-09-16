5e963466a1041decb6226b4a63e5e35e16511988
eui20n <eui20n@naver.com>
2022-09-16 11:28:00 +0900

    3주차

--- 3주차/시각.py
+++ 3주차/시각.py
@@ -2,6 +2,24 @@ def clock():
     """ 시각 함수 """
     N = int(input())
 
+    result = 0
+
+    for hour in range(0, N + 1):
+        for minute in range(60):
+            for second in range(60):
+                if "3" in str(hour):
+                    result += 1
+                    continue
+
+                if "3" in str(minute):
+                    result += 1
+                    continue
+
+                if "3" in str(second):
+                    result += 1
+
+    print(result)
+
 
 if __name__ == "__main__":
     clock()
