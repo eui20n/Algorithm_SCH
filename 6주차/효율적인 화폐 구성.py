63cb10d35c4a8b413b391a54cacd986ab92463da
eui20n <eui20n@naver.com>
2022-10-19 12:09:02 +0900

    DP - 효율적인 화폐 구성

--- 6주차/효율적인 화폐 구성.py
+++ 6주차/효율적인 화폐 구성.py
@@ -1,17 +1,30 @@
 def dp():
-    dp_table = [[float('inf') for _ in range(M + 1)] for _ in range(N)]
+    Inf = float('inf')
 
+    dp_table = [Inf for _ in range(M + 1)]
 
+    start_num = min(dollars)
 
+    for idx in dollars:
+        dp_table[idx] = 1
+
+    for idx in range(start_num, M + 1):
+        for dollar in dollars:
+            dp_table[idx] = min(dp_table[idx - dollar] + 1, dp_table[idx])
+
+    result = dp_table[M] if dp_table[M] != Inf else -1
+
+    return result
 
 
 if __name__ == '__main__':
     N, M = map(int, input().split())
-    dollars = [0]
+    dollars = []
     for _ in range(N):
         temp = int(input())
         dollars += [temp]
 
-    print(dp())
-
-# 배낭 문제의 응용
+    if max(dollars) > M:
+        print(-1)
+    else:
+        print(dp())
